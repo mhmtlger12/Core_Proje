6c2c4126c9838030d859fa154068aafcc3ac55d2
Mehmet Güler <mehmet.guler@dpu.edu.tr>
2024-06-04 14:00:32 +0300

    f

---
+++ Core_Proje/Controllers/SkillController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Yetenekler Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenekler Listesi";
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenekler Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenekler Ekleme";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);  
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var skill = skillManager.TGetByID(id);
+
+            if (skill == null)
+            {
+                return NotFound(); // Yeteneğin mevcut olmadığı durumu ele al
+            }
+
+            return View("DeleteConfirmation", skill); // Onay görünümüne yönlendir
+        }
+
+        [HttpPost]
+        public IActionResult DeleteSkillConfirmed(int id)
+        {
+            var skill = skillManager.TGetByID(id);
+
+            if (skill == null)
+            {
+                return NotFound(); // Becerinin mevcut olmadığı durumlarda durumu ele alın (açıklık sağlamak için gereksizdir)
+            }
+
+            skillManager.TDelete(skill);
+            return RedirectToAction("Index");
+        }
+    }
+}
